ce224ae95ec7a48b1bfc001e22d3ba7f4b67e1cf
Valdener Torres <torresvaldener@gmail.com>
2023-01-21 23:16:10 -0300

    add exercicios

---
+++ ex004.py
@@ -0,0 +1,5 @@
+n = input('Digite algo: ')
+print('É numérico?',n.isnumeric())
+print('É alfa numérico?',n.isalnum())
+print('É letra minúscula?',n.islower())
+print('É printável?',n.isprintable())
---
+++ scripts-py/desafio02.py
@@ -0,0 +1,4 @@
+dia = input('Qual o seu Dia de Nascimento? ')
+mes = input('Qual o seu Mês de Nascimento? ')
+ano = input('Qual o seu Ano de Nascimento? ')
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'. Correto?')
