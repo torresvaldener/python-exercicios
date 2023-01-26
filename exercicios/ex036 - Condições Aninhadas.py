1ebfe65d817bd19b931480404dc7e9a6aa0697cc
Valdener <torresvaldener@gmail.com>
2023-01-25 22:27:11 -0300

    update ex036

---
+++ exercicios/ex036 - Condições Aninhadas.py
@@ -0,0 +1,3 @@
+casa = float(input('Qual o valor da casa? '))
+salario = float(input('Qual o seu salário? '))
+anos = float(input('Quantos anos você pretende pagar? '))
--- exercicios/ex036.py
+++
@@ -1,1 +0,0 @@
-print('\033[7;32mVerdadeiro\033[m ou \033[1;31mFalso\033[m ?')
